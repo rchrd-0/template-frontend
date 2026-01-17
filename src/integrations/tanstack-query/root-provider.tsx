fa2ddc7fd7bc9eefc61b6d8a009ee74f3a0d4102
rchrd <26688060+rchrd-0@users.noreply.github.com>
2026-01-17 14:11:31 +0800

    chore(lib): tanstack query

---
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -0,0 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools as QueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 2,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
+
+export const TanstackQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <QueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { TanstackQueryProvider } from "@/integrations/tanstack-query/root-provider.tsx";
 import App from "./App.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: entry point
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <TanstackQueryProvider>
+      <App />
+    </TanstackQueryProvider>
   </StrictMode>,
 );
