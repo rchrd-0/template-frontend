b01577e4ad17f718ddcaa36770475ddf1f12047f
rchrd <26688060+rchrd-0@users.noreply.github.com>
2026-01-17 14:42:54 +0800

    chore: vite proxy, api env

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,12 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001", // Default backend port
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
