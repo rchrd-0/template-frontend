f02f17b497c43a78ccb66b67abee95404c5f6d78
rchrd <26688060+rchrd-0@users.noreply.github.com>
2026-01-17 14:54:23 +0800

    chore: spinner, sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Wrapper } from "@/components/layout/Wrapper";
+import { PageLayout } from "@/components/layout/PageLayout";
 
 export function App() {
   return (
-    <Wrapper>
+    <PageLayout>
       <main className="flex flex-1 items-center justify-center">
         <h1 className="font-semibold text-2xl">Scaffold</h1>
       </main>
-    </Wrapper>
+    </PageLayout>
   );
 }
 
--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -1,16 +1,18 @@
+import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/utils/tailwind";
 
-export function Wrapper({ className, ...props }: React.ComponentProps<"div">) {
+export function PageLayout({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div className="w-full bg-background">
       <div
-        data-slot="wrapper"
+        data-slot="page-layout"
         className={cn(
           "mx-auto flex min-h-screen w-full min-w-0 max-w-5xl flex-col gap-8 p-4 sm:gap-12 sm:p-6 lg:p-12 2xl:max-w-6xl",
           className,
         )}
         {...props}
       />
+      <Toaster />
     </div>
   );
 }
